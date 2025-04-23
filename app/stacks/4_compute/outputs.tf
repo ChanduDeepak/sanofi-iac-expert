0241560df680eba24108c1a5ac873e647fb4959c
ChanduDeepak <51658737+ChanduDeepak@users.noreply.github.com>
2025-04-23 17:19:53 +0530

    Update outputs.tf

--- app/stacks/4_compute/outputs.tf
+++ app/stacks/4_compute/outputs.tf
@@ -1,9 +1,19 @@
-output "vm_names" {
-  description = "List of VM names"
-  value       = module.virtual_machines.vm_names
+output "linux_vm_names" {
+  description = "Names of the Linux VMs"
+  value       = [for vm in azurerm_linux_virtual_machine.linux_vms : vm.name]
 }
 
-output "vm_private_ips" {
-  description = "Private IPs of the deployed VMs"
-  value       = module.virtual_machines.private_ips
+output "linux_vm_private_ips" {
+  description = "Private IPs of the Linux VMs"
+  value       = [for nic in azurerm_network_interface.linux_nics : nic.private_ip_address]
+}
+
+output "windows_vm_names" {
+  description = "Names of the Windows VMs"
+  value       = [for vm in azurerm_windows_virtual_machine.windows_vms : vm.name]
+}
+
+output "windows_vm_private_ips" {
+  description = "Private IPs of the Windows VMs"
+  value       = [for vm in azurerm_network_interface.windows_nics : vm.private_ip_address]
 }
