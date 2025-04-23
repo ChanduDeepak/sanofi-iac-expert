62cd9d56fd85088451aa8858f27334b6e953dc0b
ChanduDeepak <51658737+ChanduDeepak@users.noreply.github.com>
2025-04-23 16:05:36 +0530

    Added options\ to CI pipelines with changes all changes changes ch ch

--- app/modules/storage/outputs.tf
+++ app/modules/storage/outputs.tf
@@ -1,7 +1,7 @@
 output "storage_account_id" {
-  value = azurerm_storage_account.this.id
+  value = azurerm_storage_account.st_account.id
 }
 
 output "storage_account_name" {
-  value = azurerm_storage_account.this.name
+  value = azurerm_storage_account.st_account.name
 }
