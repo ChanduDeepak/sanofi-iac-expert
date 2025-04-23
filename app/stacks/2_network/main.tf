85182280770a3d38bf21841eceb92a842fcd5326
ChanduDeepak <51658737+ChanduDeepak@users.noreply.github.com>
2025-04-23 16:50:17 +0530

    Update main.tf

--- app/stacks/2_network/main.tf
+++ app/stacks/2_network/main.tf
@@ -1,5 +1,5 @@
 module "virtual_network" {
-  source              = "../../modules/virtual_network"
+  source              = "../../modules/vnet"
   resource_group_name = var.resource_group_name
   location            = var.location
   vnet_name           = var.vnet_name
