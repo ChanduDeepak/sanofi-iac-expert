b18478cdd432797488ac92fe5479276e1b18bf22
ChanduDeepak <51658737+ChanduDeepak@users.noreply.github.com>
2025-04-23 16:50:42 +0530

    Update outputs.tf

--- app/stacks/2_network/outputs.tf
+++ app/stacks/2_network/outputs.tf
@@ -1,9 +1,9 @@
 output "vnet_id" {
-  value       = module.virtual_network.vnet_id
+  value       = module.vnet.vnet_id
   description = "ID of the virtual network"
 }
 
 output "subnet_ids" {
-  value       = module.virtual_network.subnet_ids
+  value       = module.vnet.subnet_ids
   description = "IDs of the subnets"
 }
