85e3a6e3de35a716ab8e16469ae3314a4e7cc1da
ChanduDeepak <51658737+ChanduDeepak@users.noreply.github.com>
2025-04-23 17:17:38 +0530

    Update variables.tf

--- app/stacks/4_compute/variables.tf
+++ app/stacks/4_compute/variables.tf
@@ -1,50 +1,41 @@
 variable "resource_group_name" {
-  description = "The name of the resource group"
+  description = "Name of the resource group"
   type        = string
 }
 
 variable "location" {
-  description = "Azure region for VM deployment"
+  description = "Azure region"
   type        = string
 }
 
-variable "vnet_name" {
-  description = "Name of the Virtual Network"
-  type        = string
-}
-
-variable "subnet_name" {
-  description = "Name of the subnet where the VMs will be placed"
-  type        = string
-}
-
-variable "admin_username" {
-  description = "Admin username for the VM"
-  type        = string
-}
-
-variable "admin_password" {
-  description = "Admin password for the VM"
-  type        = string
-  sensitive   = true
-}
-
-variable "vm_count" {
-  description = "Number of VMs to deploy"
+variable "linux_vm_count" {
+  description = "Number of Linux VMs"
   type        = number
+  default     = 4
+}
+
+variable "linux_vm_name_prefix" {
+  description = "Prefix for Linux VM names"
+  type        = string
+  default     = "linuxvm"
+}
+
+variable "windows_vms" {
+  description = "Map of Windows VM names to config"
+  type = map(object({
+    admin_username = string
+    admin_password = string
+  }))
+  default = {}
 }
 
 variable "vm_size" {
-  description = "Size of the virtual machines (e.g., Standard_DS1_v2)"
+  description = "Size for the VMs"
   type        = string
+  default     = "Standard_D4s_v3"
 }
 
-variable "vm_name_prefix" {
-  description = "Prefix to use for naming VMs"
+variable "subnet_id" {
+  description = "Subnet ID to place the VMs"
   type        = string
-}
-
-variable "tags" {
-  description = "A map of tags for resources"
-  type        = map(string)
 }
