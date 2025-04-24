46881704d78f654425a081c6790e1741c7ea3db2
ChanduDeepak <51658737+ChanduDeepak@users.noreply.github.com>
2025-04-24 16:32:45 +0530

    Update variables.tf

--- app/modules/vms/variables.tf
+++ app/modules/vms/variables.tf
@@ -1,57 +1,57 @@
 variable "vm_type" {
-  description = "Type of VM: linux or windows"
+  description = "VM type: linux or windows"
   type        = string
 }
 
 variable "location" {
-  description = "Azure region"
-  type        = string
+  type = string
 }
 
 variable "resource_group_name" {
-  description = "Name of the resource group"
-  type        = string
+  type = string
 }
 
 variable "subnet_id" {
-  description = "Subnet ID to attach the VMs"
-  type        = string
+  type = string
 }
 
 variable "vm_size" {
-  description = "Size of the virtual machines"
-  type        = string
-  default     = "Standard_D4s_v3"
+  type = string
 }
 
 # Linux VMs
 variable "linux_vm_count" {
-  description = "Number of Linux VMs"
-  type        = number
-  default     = 0
+  type    = number
+  default = 0
 }
 
 variable "linux_vm_names" {
-  description = "List of Linux VM names"
-  type        = list(string)
-  default     = []
+  type    = list(string)
+  default = []
+}
+
+variable "linux_admin_username" {
+  type    = string
+  default = null
+}
+
+variable "linux_admin_password" {
+  type    = string
+  default = null
 }
 
 # Windows VMs
 variable "windows_vm_map" {
-  description = "Map of Windows VM names to properties (can be empty strings if unused)"
-  type        = map(any)
-  default     = {}
+  type    = map(any)
+  default = {}
 }
 
-# Common credentials
-variable "admin_username" {
-  description = "Admin username for all VMs"
-  type        = string
+variable "windows_admin_username" {
+  type    = string
+  default = null
 }
 
-variable "admin_password" {
-  description = "Admin password for all VMs"
-  type        = string
-  sensitive   = true
+variable "windows_admin_password" {
+  type    = string
+  default = null
 }
