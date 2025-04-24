3db64bec5869ef2354062486947872d620c4378a
ChanduDeepak <51658737+ChanduDeepak@users.noreply.github.com>
2025-04-24 17:04:45 +0530

    Update main.tf

--- app/modules/vms/main.tf
+++ app/modules/vms/main.tf
@@ -1,23 +1,32 @@
 resource "azurerm_linux_virtual_machine" "linux" {
-  count               = var.vm_type == "linux" ? var.linux_vm_count : 0
+  count               = var.linux_vm_count
   name                = var.linux_vm_names[count.index]
   location            = var.location
   resource_group_name = var.resource_group_name
   size                = var.vm_size
   admin_username      = var.linux_admin_username
   admin_password      = var.linux_admin_password
-  network_interface_ids = [/* your NIC logic */]
-  # Add image etc
-}
+  network_interface_ids = [/* add NIC ID here or remove if not yet available */]
 
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "osdisk-${count.index}"
+  }
+}
 resource "azurerm_windows_virtual_machine" "windows" {
-  for_each            = var.vm_type == "windows" ? var.windows_vm_map : {}
+  for_each            = var.windows_vm_map
   name                = each.value.name
   location            = var.location
   resource_group_name = var.resource_group_name
   size                = var.vm_size
   admin_username      = var.windows_admin_username
   admin_password      = var.windows_admin_password
-  network_interface_ids = [/* your NIC logic */]
-  # Add image etc
+  network_interface_ids = [/* add NIC ID here or remove if not yet available */]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "${each.value.name}-osdisk"
+  }
 }
