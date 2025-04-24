52cc3eba2adcdda033d6f278c421f596fdaf9dde
ChanduDeepak <51658737+ChanduDeepak@users.noreply.github.com>
2025-04-24 17:56:34 +0530

    Update main.tf

--- app/stacks/4_compute/main.tf
+++ app/stacks/4_compute/main.tf
@@ -1,19 +1,15 @@
 module "vms" {
   source              = "../../modules/vms"
-  vm_type             = "all" # You can set to "all", "linux", or "windows" depending on your use case
   location            = var.location
   resource_group_name = var.resource_group_name
-  subnet_id           = var.subnet_id
   vm_size             = var.vm_size
 
-  # Linux-specific
-  linux_vm_count        = var.linux_vm_count
-  linux_vm_names        = var.linux_vm_names
-  linux_admin_username  = var.linux_admin_username
-  linux_admin_password  = var.linux_admin_password
+  linux_vm_count          = var.linux_vm_count
+  linux_vm_names          = var.linux_vm_names
+  linux_admin_username    = var.linux_admin_username
+  linux_admin_password    = var.linux_admin_password
 
-  # Windows-specific
-  windows_vm_map        = var.windows_vm_map
-  windows_admin_username = var.windows_admin_username
-  windows_admin_password = var.windows_admin_password
+  windows_vm_map          = var.windows_vm_map
+  windows_admin_username  = var.windows_admin_username
+  windows_admin_password  = var.windows_admin_password
 }
