dfa4473860b48777b5cbecfb2d18e7c0f3ce6271
ChanduDeepak <51658737+ChanduDeepak@users.noreply.github.com>
2025-04-28 15:40:44 +0530

    Added new branch and removed the compute in main as well and modified few

--- app/stacks/1_rg/outputs.tf
+++ app/stacks/1_rg/outputs.tf
@@ -1,14 +1,14 @@
 output "resource_group_name" {
   description = "The name of the resource group"
-  value       = azurerm_resource_group.this.name
+  value       = azurerm_resource_group.rg.name
 }
 
 output "resource_group_location" {
   description = "The location of the resource group"
-  value       = azurerm_resource_group.this.location
+  value       = azurerm_resource_group.rg.location
 }
 
 output "resource_group_id" {
   description = "The ID of the resource group"
-  value       = azurerm_resource_group.this.id
+  value       = azurerm_resource_group.rg.id
 }
