60c40bd11dd8e0efe74e57c9e0c56c4125972bf9
ChanduDeepak <51658737+ChanduDeepak@users.noreply.github.com>
2025-04-28 15:34:56 +0530

    Added new branch and removed the compute in main as well

--- app/stacks/4_compute/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "linux_vm_names" {
-  description = "Names of the Linux VMs"
-  value       = var.linux_vm_count > 0 ? module.linux_vms.vm_names : []
-}
-
-output "windows_vm_names" {
-  description = "Names of the Windows VMs"
-  value       = length(var.windows_vm_map) > 0 ? module.windows_vms.vm_names : []
-}
